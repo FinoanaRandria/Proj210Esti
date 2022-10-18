c5baa393e4ea523f31604a3aeaeef79c33040393
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-10-18 20:52:45 +0300

    Login vao2

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -25,23 +25,7 @@ function Login() {
                </div>
                     <input type="submit" value="Login" className="btn solid"/>
 
-                    <p className="social-text">Or Sign In With Social Platform</p>
-                  <div className="social-media">
-
-                    <a href="#" className="social-icon">
-                        <i className="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="#" className="social-icon">
-                      <i className="fab fa-twitter"></i>
-                    </a>
-                    <a href="#" className="social-icon">
-                      <i className="fab fa-google"></i>
-                    </a>
-                    
-                    <a href="#" className="social-icon">
-                      <i className="fab fa-linkedin-in"></i>
-                    </a>
-                  </div>
+                  
                   
 
             </form>
@@ -66,19 +50,7 @@ function Login() {
                      <p className="social-text">Or Sign Up With Social Platform</p>
                    <div className="social-media">
  
-                     <a href="#" className="social-icon">
-                         <i className="fab fa-facebook-f"></i>
-                     </a>
-                     <a href="#" className="social-icon">
-                       <i className="fab fa-twitter"></i>
-                     </a>
-                     <a href="#" className="social-icon">
-                       <i className="fab fa-google"></i>
-                     </a>
                      
-                     <a href="#" className="social-icon">
-                       <i className="fab fa-linkedin-in"></i>
-                     </a>
                    </div>
                    
  
